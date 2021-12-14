e51890ca89f908822b1bbddae02559c72e37f309
Adrián Antón <adrtler@gmail.com>
2021-12-14 12:52:12 +0100

    Pruebas sobre ProductImage

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductCard, ProductImage } from '../../src/components';
+import { product2 } from '../data/products';
+
+describe('ProductImage', () => {
+  test('Debe mostrar el componente de la imagen pasada como parámetro', () => {
+    const wrapper = renderer.create(
+      <ProductImage img="./coffee-mug-prueba.png" />
+    );
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  test('debe de mostrar el componente con la imagen del producto', () => {
+    const wrapper = renderer.create(
+      <ProductCard product={product2}>{() => <ProductImage />}</ProductCard>
+    );
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+});
